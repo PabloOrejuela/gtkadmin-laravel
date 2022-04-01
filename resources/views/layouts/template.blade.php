c330287fbd05fb04ef25f9dfd2865c1e052c424f
Pablo Orejuela <pabloorejuela@hotmail.com>
2022-04-01 14:35:17 -0500

    Cambio del logo al login

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{asset('css/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
+    {{-- <link href="{{asset('css/bootstrap.min.css')}}" type="text/css" rel="stylesheet"> --}}
     <link href="{{asset('css/styles.css')}}" type="text/css" rel="stylesheet">
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    {{-- <script src="{{asset('js/bootstrap.min.js')}}"></script> --}}
     <title>@yield('title')</title>
     <style>
         .active {
